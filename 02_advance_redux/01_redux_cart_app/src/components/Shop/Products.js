a40d3daf8e2ff8cdd3f2c87b6a741594764bfa1b
Aritra Nandi <aritranandi30@gmail.com>
2024-01-11 23:12:28 +0530

    add to cart and increasing the quantity on cart button

--- 02_advance_redux/01_redux_cart_app/src/components/Shop/Products.js
+++ 02_advance_redux/01_redux_cart_app/src/components/Shop/Products.js
@@ -2,15 +2,35 @@ import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const Products = (props) => {
+
+  const DUMMY_PRODUCTS = [
+    {
+      id: 1,
+      name: 'Book1',
+      price: 10,
+      description: 'First book I wrote'
+    },
+    {
+      id: 2,
+      name: 'Book2',
+      price: 12,
+      description: 'Second book I wrote'
+    }
+  ]
+
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map(item => 
+          <ProductItem
+            key={item.id}
+            id={item.id}
+            title={item.name}
+            price={item.price}
+            description={item.description}
+          />
+        )}
       </ul>
     </section>
   );
