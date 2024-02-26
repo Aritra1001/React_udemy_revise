e93aaaca20830cc101c9d866b6969a8d0b6291a5
Aritra <aritranandi30@gmail.com>
2024-02-26 23:14:28 +0530

    sending cart data to backend, handling responses,

--- 02_advance_redux/01_redux_cart_app/src/App.js
+++ 02_advance_redux/01_redux_cart_app/src/App.js
@@ -1,27 +1,95 @@
-import { useEffect } from 'react';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useEffect } from "react";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector, useDispatch } from "react-redux";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+  const dispatch = useDispatch();
 
-  const showCart = useSelector(state=> state.ui.cartIsVisible);
-  const cart = useSelector(state =>state.cart);
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "Sending...",
+          title: "Sending!",
+          message: "Sending Cart Data..",
+        })
+      );
+      const response = await fetch(
+        "https://cart-b3957-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
 
-  useEffect(()=>{
-    fetch('https://cart-b3957-default-rtdb.firebaseio.com/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart)
-    })
-  }, [cart])
+      if (!response.ok) {
+        throw new Error("Sending Cart Data Failed");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Successful!",
+          message: "Cart Data Sent Successfully..",
+        })
+      );
+    };
+
+    if(isInitial){
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((err) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Some error occurred",
+          message: "Cart data not sent...",
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-     {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
 export default App;
+
+// Where to put our logic for side effects/ backend code?
+// Fat reducers vs Fat components vs Fat Actions.
+
+/** Synchronous code/ code without side effcts/ data transformations - Inside reducers,
+ * Avoid action creators or components */
+
+/** Async code/ code with side effects - Inside components or action creators
+ * Never use reducers */
+
+/** Now to sync our new state with the server/ update the server from the frontend with the new state that we derived,
+ * we must follow these steps
+ * 1. We can first do the data transformation work on the frontend in the reducer and let redux update its store.
+ * 2. We send the request to the server from the component ProductItem.js file or App.js file.
+ */
--- 02_advance_redux/01_redux_cart_app/src/store/ui-slice.js
+++ 02_advance_redux/01_redux_cart_app/src/store/ui-slice.js
@@ -3,14 +3,23 @@ import { createSlice } from '@reduxjs/toolkit';
 const uiSlice = createSlice({
     name: 'ui',
     initialState: {
-        cartIsVisible: false
+        cartIsVisible: false,
+        notification:  null
     },
     reducers:{
         toggle(state){
             state.cartIsVisible = !state.cartIsVisible;
+        },
+        showNotification(state, action){
+            state.notification = {
+                status: action.payload.status,
+                title: action.payload.title,
+                message: action.payload.message
+            }
         }
     }
 })
+// In the reducer change the state into an object which consists of status, titlke and message
 
 export const uiActions = uiSlice.actions;
 
