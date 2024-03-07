730a968e11063374bfcb366c60dad75f7286feb7
Aritra <aritranandi30@gmail.com>
2024-03-07 22:41:08 +0530

    NavLink, useNavigate,

--- 03_react_router/01_basic_routing/src/Components/MainNavigation.js
+++ 03_react_router/01_basic_routing/src/Components/MainNavigation.js
@@ -1,20 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import classes from './MainNavigation.module.css';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
+
+/**NavLink has a special behaviour, it's className property does not a take a string like normal className
+ * instead takes a function and the function should return the CSS classname that should be added to the anchor tag
+ * That function automatically receives an object which we can destructure to get the isActive property.
+ * The object with the isActive property is provided by the react-router-dom.
+ * isActive is a boolean which is true if this link is currently active or false, if it does not lead
+ * to the currently active route.
+ * */
   return (
     <>
-        <header className={classes.header}>
-            <nav>
-                <ul className={classes.list}>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/products'>Products</Link></li>
-                </ul>
-            </nav>
-        </header>
+      <header className={classes.header}>
+        <nav>
+          <ul className={classes.list}>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+                // end
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/products"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                Products
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default MainNavigation
+export default MainNavigation;
--- 03_react_router/01_basic_routing/src/Pages/HomePage.js
+++ 03_react_router/01_basic_routing/src/Pages/HomePage.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const HomePage = () => {
+// useNavigate hook is used to navigate programmatically
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate('/products');
+  }
+
   return (
     <>
     <h1>My Home Page</h1>
     <p>go to <Link to='/products'>my products page</Link></p>
+    <button onClick={navigateHandler}>Navigate</button>
     </>
   )
 }
