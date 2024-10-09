722ff80e786b7968d39064690d71b463a8c48bd5
Aritra Nandi <aritranandi30@gmail.com>
2024-10-09 18:12:03 +0530

    part of last commit

---
+++ 05_nextjs/02_meals-app/lib/meals.js
@@ -0,0 +1,12 @@
+// Code that will reach out to database and fetch the data
+import sql from "better-sqlite3";
+
+const db = sql('meals.db');
+
+export const getMeals = async() => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    return db.prepare("SELECT * FROM meals").all();
+    // all() - It is used when we are fetching data and we want all the rows
+    // get() - for getting single row of the data
+    // run() - when we are changing data
+}
