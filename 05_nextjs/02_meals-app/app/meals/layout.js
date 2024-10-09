9be1256277458c9127de37ebc3d7e987bd6f7bb2
Aritra Nandi <aritranandi30@gmail.com>
2024-10-09 15:17:56 +0530

    sql db for meals, fetching data from meals in lib, using meals data in meals page

--- 05_nextjs/02_meals-app/app/meals/layout.js
+++ 05_nextjs/02_meals-app/app/meals/layout.js
@@ -3,7 +3,6 @@ import React from 'react'
 const MealsLayout = ({children}) => {
   return (
     <>
-        <p>Meals Layout</p>
         {children}
     </>
   )
--- 05_nextjs/02_meals-app/app/meals/page.js
+++ 05_nextjs/02_meals-app/app/meals/page.js
@@ -1,21 +1,29 @@
 import Link from "next/link";
 import React from "react";
-import classes from './page.module.css';
+import classes from "./page.module.css";
 import MealsGrid from "@/components/meal/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-const MealsPage = () => {
-  return <>
+// Serer components cam be converted to async/await.
+const MealsPage = async () => {
+  let meals = await getMeals();
+  return (
+    <>
       <header className={classes.header}>
-        <h1>Delicious meals, created <span className={classes.highlight}>by you</span></h1>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
         <p>Choose your favourite recipe and cook it yourself. It is fun!!</p>
         <p className={classes.cta}>
-          <Link href='/meals/share'>Share your favorite recipe</Link>
+          <Link href="/meals/share">Share your favorite recipe</Link>
         </p>
       </header>
-      <main className={classes.main}>
-        <MealsGrid meals={[]}/>
+      <main>
+        <MealsGrid meals={meals} />
       </main>
-  </>;
+    </>
+  );
 };
 
 export default MealsPage;
