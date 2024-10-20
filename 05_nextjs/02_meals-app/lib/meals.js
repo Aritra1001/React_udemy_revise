e3cfb8ec3a914c1698d992d064c9d0202173f3b0
Aritra Nandi <aritranandi30@gmail.com>
2024-10-20 19:20:46 +0530

    error handling, not found page

--- 05_nextjs/02_meals-app/lib/meals.js
+++ 05_nextjs/02_meals-app/lib/meals.js
@@ -5,6 +5,7 @@ const db = sql('meals.db');
 
 export const getMeals = async() => {
     await new Promise((resolve) => setTimeout(resolve, 2000));
+    // throw new Error('Loading meals failed');
     return db.prepare("SELECT * FROM meals").all();
     // all() - It is used when we are fetching data and we want all the rows
     // get() - for getting single row of the data
