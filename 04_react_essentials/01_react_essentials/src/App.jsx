43160df88cdb115add4df6a8882c04b8bf7dc699
Aritra Nandi <aritranandi30@gmail.com>
2025-01-06 18:19:53 +0530

    different tabs for different concepts

--- 04_react_essentials/01_react_essentials/src/App.jsx
+++ 04_react_essentials/01_react_essentials/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/header/Header";
 import CoreConcepts from "./components/coreConcepts/CoreConcepts";
 import { CORE_CONCEPTS } from "./data";
+import TabButton from "./components/tabButton/TabButton";
 
 function App() {
   return (
@@ -20,6 +21,16 @@ function App() {
             <CoreConcepts {...CORE_CONCEPTS[2]} />
             <CoreConcepts {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton >Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>States</TabButton>
+            <TabButton>Props</TabButton>
+          </menu>
+          Some Dynamic Content
         </section>
       </main>
     </div>
